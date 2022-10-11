1557bcbfe90acfce3e7949cad3df1333c1462419
Soroush Safarkhah <soroush_Raw@hotmail.com>
2022-10-11 11:31:36 +0200

    link update

--- src/pages/Works/Works.jsx
+++ src/pages/Works/Works.jsx
@@ -164,7 +164,7 @@ export const Works = () => {
           <ScrollAnimation animateIn="fadeInRight" duration={1.5}>
           <Row className="works-wrapper">
             <Col md={4}>
-              <div className="works-info" href="https://github.com/sorousch9/portfolio" rel="noreferrer" target="_blank">
+              <a className="works-info" href="https://github.com/sorousch9/portfolio" rel="noreferrer" target="_blank">
                 <h3>Shi beauty</h3>
                 <h6>React Store App</h6>
                 <div className="dependencies-ico">
@@ -205,7 +205,7 @@ export const Works = () => {
                     </span>
                   </i>
                 </div>
-              </div>
+              </a>
             </Col>
             <Col md={8}>
               <div className="works-img">
